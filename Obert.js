0d5fc9068da6ca9313f184efc2b447c495954b74
Emanuel de Jong <44051853+killbottt@users.noreply.github.com>
2019-12-10 17:34:39 +0100

    more potion

--- Obert.js
+++ Obert.js
@@ -29,13 +29,11 @@ setInterval(function(){
 				smart_move("Town");
 			}
 			else if(is_in_range("Town")){
-				//open stand
+				parent.socket.emit("merchant",{close:1});
 				marketing = true;
 			}
 		}
 	}
-	
-	
 },1000/4);
 
 
@@ -51,43 +49,55 @@ function on_cm(name, data){
 
 var potions_needed = {};
 for(i = 0; i < player_names.length; i++){
-	potions_needed[player_names[i]] = [0, 0];
+	potions_needed[player_names[i]] = [["", 0], ["", 0]]
 }
-var minimum_hp_potions = 300;
+
+var min_hp_potions = 150;
 var hp_before_potion_type_change = 1500;
-var minimum_mp_potions = 300;
+var hp_potion_overshoot = 150;
+
+var mini_mp_potions = 150;
 var mp_before_potion_type_change = 1000;
+var mp_potion_overshoot = 150;
 setInterval(function(){
 	var player_needs_potion = false;
 	for(i = 0; i < player_names.length; i++){
 		var player = window(player_names[i]);
 
-		var hp_potion_type;
-		if(player.max_hp >= hp_before_potion_type_change) hp_potion_type = "hpot1";
-		else hp_potion_type = "hpot0";
-
-		var mp_potion_type;
-		if(player.max_mp >= mp_before_potion_type_change) mp_potion_type = "mpot1";
-		else hp_potion_type = "mpot0";
-
 		var hp_potions_needed;
 		var mp_potions_needed;
 		for(j = 0; j < player.items.length; j++){
-			if(player.items[i]["name"] == hp_potion_type){
-				hp_potions_needed = minimum_hp_potions - player.items[i]["q"];
+			var item = player.items[j];
+			if(item == null) continue;
+			if(item["name"] == hp_potion_type){
+				hp_potions_needed = min_hp_potions - item["q"];
+				hp_potions_needed += hp_potion_overshoot;
 			}
-			if(player.items[i]["name"] == mp_potion_type){
-				mp_potions_needed = minimum_mp_potions - player.items[i]["q"];
+			if(item["name"] == mp_potion_type){
+				mp_potions_needed = min_mp_potions - item["q"];
+				mp_potions_needed += mp_potion_overshoot;
 			}
 		}
 
 		if(!hp_potions_needed || hp_potions_needed > 0){
 			player_needs_potion = true;
-			potions_needed[player.name][0] = hp_potions_needed;
+
+			var hp_potion_type;
+			if(player.max_hp >= hp_before_potion_type_change) hp_potion_type = "hpot1";
+			else hp_potion_type = "hpot0";
+
+			potions_needed[player.name][0][0] = hp_potion_type;
+			potions_needed[player.name][0][1] = hp_potions_needed;
 		}
 		if(!mp_potions_needed || mp_potions_needed > 0){
 			player_needs_potion = true;
-			potions_needed[player.name][1] = mp_potions_needed;
+
+			var mp_potion_type;
+			if(player.max_mp >= mp_before_potion_type_change) mp_potion_type = "mpot1";
+			else hp_potion_type = "mpot0";
+
+			potions_needed[player.name][1][0] = hp_potion_type;
+			potions_needed[player.name][1][1] = hp_potions_needed;
 		}
 	}
 
