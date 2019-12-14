4a969d1e1a99655a9ed08a8aa928d3c870fc5013
Emanuel de Jong <44051853+killbottt@users.noreply.github.com>
2019-12-14 12:08:20 +0100

    ...

--- update_players.js
+++ update_players.js
@@ -3,13 +3,9 @@ setInterval(function(){
 	for(player_name in players){
 		var player = get_player(player_name);
 		if(!player){
-			var player_from_party = parent.party[player_name];
-			players[player_name] = player_from_party;
-			window[player_name] = player_from_party;
+			var player = parent.party[player_name];
 		}
-		else{
-			players[player_name] = player;
-			window[player_name] = player;
-		}
+		players[player_name] = player;
+		window[player_name] = player;
 	}
 },1000*10);
